dac6e3043757b6ba53f1ad71dec80bbcfbb82aa7
kt <98679116+Kartikey-Tiwari@users.noreply.github.com>
2022-10-17 20:10:25 +0530

    Add click and drag to color black functionality

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const board = document.querySelector('#board');
 
+function colorDivNormal(event){
+    event.preventDefault();
+    if (event.buttons === 1)
+        event.target.style.backgroundColor = 'black';
+}
+
 for(let j = 0; j < 64; j++){
     const row = document.createElement('div');
     row.style.width = '100%';
@@ -7,6 +13,8 @@ for(let j = 0; j < 64; j++){
     row.style.display = 'flex';
     for(let i = 0; i < 64; i++){
         const div = document.createElement('div');
+        div.addEventListener('mouseover', colorDivNormal);
+        div.addEventListener('mousedown', colorDivNormal);
         div.style.width = `${100/64}%`;
         div.style.height = '100%';
         row.appendChild(div);
